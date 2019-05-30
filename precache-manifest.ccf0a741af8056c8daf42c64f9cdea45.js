1375c2ffd9aa6f566e43ac3864fb3c700bf65ed7
yanal otaibi <yanalotaibi@gmail.com>
2019-05-30 03:01:35 +0300

    Updates

---
+++ precache-manifest.ccf0a741af8056c8daf42c64f9cdea45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "805d989128f8c1dbfeef",
+    "url": "/React-Maps/static/js/runtime~main.4a90b8e8.js"
+  },
+  {
+    "revision": "5ea124d22810da9def16",
+    "url": "/React-Maps/static/js/main.5faad473.chunk.js"
+  },
+  {
+    "revision": "59532dae5dbff3462e3f",
+    "url": "/React-Maps/static/js/2.d5a12fb0.chunk.js"
+  },
+  {
+    "revision": "5ea124d22810da9def16",
+    "url": "/React-Maps/static/css/main.52161ca6.chunk.css"
+  },
+  {
+    "revision": "05f8efe6ea227d725475017436fb6f84",
+    "url": "/React-Maps/index.html"
+  }
+];
